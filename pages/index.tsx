6694c5df57533ed444ceb0395b61f6c139d839b2
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-05-20 10:49:12 -0300

    feat: projeto atualizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import Box from "../src/components/Box";
-import theme from "../src/theme/theme";
-
+import Box from "@src/components/Box";
+import theme from "@src/theme/theme";
 export default function HomeScreen() {
   return (
     <Box
       tag="main"
-      stylesheet={{
+      styleSheet={{
         fontFamily: theme.typography.fontFamily,
+        backgroundColor: {
+          xs: 'red',
+          sm: 'yellow',
+          md: 'blue',
+        }
       }}
     >
       Oi
     </Box>
-  );
+  )
 }
